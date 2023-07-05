ae38ab23ffa1e8bd4ce63ce2e39091f92ac59e26
rafidmahin20 <submanmahin@gmail.com>
2023-07-05 13:12:46 +0600

    edited

--- proshot-corporate/src/App.js
+++ proshot-corporate/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from "react";
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
     </div>
   );
 }
